60a8c7d69e5a6a03e874b056a1fc92cd404f7d76
kcft99 <ktang.consultant@yahoo.com>
2018-11-13 10:54:30 -0500

    Update MyTestNgServerApplication.java

--- src/main/java/com/ngserver/service/MyTestNgServerApplication.java
+++ src/main/java/com/ngserver/service/MyTestNgServerApplication.java
@@ -2,11 +2,19 @@ package com.ngserver.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyTestNgServerApplication {
+public class MyTestNgServerApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyTestNgServerApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyTestNgServerApplication.class);
+    }
+
 }
